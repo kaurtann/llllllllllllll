e339b92975ee3737944ec2f090c1de5ba58a62cd
kaurtann <52679233+kaurtann@users.noreply.github.com>
2019-07-28 22:59:17 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,38 @@
+import{songInfo}from '../../app/PunjabiSongs';
+export const DetailsOfSongs:
+songInfo[]=[{
+    songName:'Roti',
+    songArtist:'Gurdas Mann',
+    songGenre:'Bread Of Life',
+    songdate: 'August 3, 2013',
+    songPicture:'assets/images/image1.jpg',
+},
+
+{
+    songName:'Yarr Anmulle',
+    songArtist:'Sharry Mann',
+    songGenre:'Friendship',
+    songdate:'February 2011',
+    songPicture:'assets/images/image2.jpg',
+},
+{
+    songName:'Abban de deso',
+    songArtist:'Amrinder Gill',
+    songGenre:'Life in foreign countries',
+    songdate:'July 24,2019',
+    songPicture:'assets/images/image3.jpg',
+},
+{
+    songName:'Badlan de Kalje',
+    songArtist:'Nimrat Khaira and Amrinder Gill',
+    songGenre:'Romantic',
+    songdate:'July 24,2019',
+    songPicture:'assets/images/image4.jpg',
+}
+
+
+
+]
+
+
+
